5cea55575178c4d22e0c292fa50246f93778d59d
zineb <zineb.boutaleb@centrale-casablanca.ma>
2022-12-30 16:13:19 +0100

    service

--- model/player.py
+++ model/player.py
@@ -2,13 +2,13 @@ from battlefield import Battlefield
 
 
 class Player:
-    def __init__(self, name: str, battle_field: Battlefield):
+    def __init__(self, name: str, battlefield: Battlefield):
         self.id = None
         self.name = name
-        self.battle_field = battle_field
+        self.battlefield = battlefield
 
     def get_name(self) -> str:
         return self.name
 
     def get_battlefield(self) -> Battlefield:
-        return self.battle_field
+        return self.battlefield
--- services/game_service.py
+++ services/game_service.py
@@ -24,10 +24,10 @@ class GameService:
         if len(game.players) >= 2:
             return False
         # Créez le nouveau joueur et ajoutez-le à la partie
-        battle_field = Battlefield(game.players[0].battle_field.min_x, game.players[0].battle_field.max_x,
-                                   game.players[0].battle_field.min_y, game.players[0].battle_field.max_y,
-                                   game.players[0].battle_field.min_z, game.players[0].battle_field.max_z)
-        player = Player(player_name, battle_field)
+        battlefield = Battlefield(game.players[0].battlefield.min_x, game.players[0].battlefield.max_x,
+                                   game.players[0].battlefield.min_y, game.players[0].battlefield.max_y,
+                                   game.players[0].battlefield.min_z, game.players[0].battlefield.max_z)
+        player = Player(player_name, battlefield)
         game.add_player(player)
         # Enregistrez la partie mise à jour dans la base de données
         self.game_dao.update_game(game)
@@ -46,12 +46,12 @@ class GameService:
         if player is None:
             return False
         # Vérifier si la position est disponible sur le champ de bataille du joueur
-        if (x < player.battle_field.min_x or x > player.battle_field.max_x or
-                y < player.battle_field.min_y or y > player.battle_field.max_y or
-                z < player.battle_field.min_z or z > player.battle_field.max_z):
+        if (x < player.battlefield.min_x or x > player.battlefield.max_x or
+                y < player.battlefield.min_y or y > player.battlefield.max_y or
+                z < player.battlefield.min_z or z > player.battlefield.max_z):
             return False
         # Vérifier si le joueur a atteint la limite de vaisseaux autorisés sur son champ de bataille
-        if len(player.battle_field.vessels) >= player.battle_field.max_hits:
+        if len(player.battlefield.vessels) >= player.battlefield.max_hits:
             return False
         # Créer le vaisseau et l'ajouter au champ de bataille du joueur
         vessel = Vessel(x, y, z, vessel_type)
@@ -60,3 +60,58 @@ class GameService:
             return False
         self.game_dao.db_session.merge(player)
         return True
+
+    def shoot_at(self, game_id: int, shooter_name: str, vessel_id: int, x: int, y: int, z: int) -> bool:
+        # Vérifier si la partie existe
+        game = self.game_dao.find_game(game_id)
+        if game is None:
+            return False
+        # Vérifier si le joueur fait partie de la partie
+        shooter = next((p for p in game.players if p.name == shooter_name), None)
+        if shooter is None:
+            return False
+        # Vérifier si le vaisseau appartient au joueur et s'il peut tirer
+        vessel = next((v for v in shooter.battlefield.vessels if v.id == vessel_id and v.can_shoot()), None)
+        if vessel is None:
+            return False
+        # Vérifier si la cible est dans la portée de l'arme du vaisseau
+        if not vessel.in_range(x, y, z):
+            return False
+        # Trouver la cible (un vaisseau adverse)
+        for p in game.players:
+            if p.name == shooter_name:
+                continue
+            target = next((v for v in p.battlefield.vessels if v.coord_x == x and v.coord_y == y and v.coord_z == z),
+                          None)
+            if target is not None:
+                break
+        # Si la cible n'a pas été trouvée, cela signifie que
+        # la coordonnée (x, y, z) ne correspond à aucun vaisseau
+        # dans la partie
+            if target is None:
+                return False
+        # Tirer sur la cible
+            vessel.fire_at(target)
+        # Mettre à jour la base de données
+        self.game_dao.db_session.merge(shooter)
+
+    def get_game_status(self, game_id: int, player_name: str) -> str:
+        game = self.game_dao.find_game(game_id)
+        if game is None:
+            return "ENCOURS"
+
+        # Vérifier si le joueur fait partie de la partie
+        player = next((p for p in game.players if p.name == player_name), None)
+        if player is None:
+            return "ENCOURS"
+
+        # Vérifier si le joueur a perdu
+        if player.battlefield.all_vessels_destroyed():
+            return "PERDU"
+
+        # Vérifier si le joueur a gagné
+        opponent = next((p for p in game.players if p.name != player_name), None)
+        if opponent is not None and opponent.battlefield.all_vessels_destroyed():
+            return "GAGNE"
+
+        return "ENCOURS"
