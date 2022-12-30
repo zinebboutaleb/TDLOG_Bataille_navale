77c17420e6a987921e3daaebe3244e8132cedfb6
zineb <zineb.boutaleb@centrale-casablanca.ma>
2022-12-30 03:06:44 +0100

    game_dao

---
+++ dao/game_dao.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+
+engine = create_engine('sqlite:////tmp/tdlog.db', echo=True, future=True)
+Base = declarative_base(bind=engine)
+Session = sessionmaker(bind=engine)
+
+
+class GameEntity(Base):
+    __tablename__ = 'game'
+    id = Column(Integer, primary_key=True)
+    players = relationship("PlayerEntity", back_populates="game", cascade="all, delete-orphan")
+
+
+class PlayerEntity(Base):
+    __tablename__ = 'player'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    game_id = Column(Integer, ForeignKey("game.id"), nullable=False)
+    game = relationship("GameEntity", back_populates="players")
+    battle_field = relationship("BattlefieldEntity",
+                                back_populates="player",
+                                uselist=False, cascade="all, delete-orphan")
