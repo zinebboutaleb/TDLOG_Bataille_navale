0f9f51ee626ef2d132fd406d6c9e75a4ac82f97d
zineb <zineb.boutaleb@centrale-casablanca.ma>
2022-12-30 16:53:29 +0100

    controller

--- controllers/game_controller.py
+++ controllers/game_controller.py
@@ -4,6 +4,7 @@ from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from model.game import Game
 from services.game_service import GameService
+
 app = FastAPI()
 game_service = GameService()
 
@@ -18,6 +19,29 @@ class CreateGameData(BaseModel):
     max_z: int
 
 
+class JoinGameData(BaseModel):
+    game_id: int
+    player_name: str
+
+
+class AddVesselData(BaseModel):
+    game_id: int
+    player_name: str
+    vessel_type: str
+    x: int
+    y: int
+    z: int
+
+
+class ShootAtData(BaseModel):
+    game_id: int
+    shooter_name: str
+    vessel_id: int
+    x: int
+    y: int
+    z: int
+
+
 @app.post("/create-game")
 async def create_game(game_data: CreateGameData):
     return game_service.create_game(game_data.player_name, game_data.min_x,
@@ -29,3 +53,36 @@ async def create_game(game_data: CreateGameData):
 @app.get("/get-game")
 async def get_game(game_id: int) -> Game:
     return game_service.get_game(game_id)
+
+
+@app.post("/join-game")
+async def join_game(game_data: JoinGameData) -> bool:
+    return game_service.join_game(game_data.game_id, game_data.player_name)
+
+
+@app.post("/add-vessel")
+async def add_vessel(game_data: AddVesselData) -> bool:
+    return game_service.add_vessel(game_data.game_id, game_data.player_name,
+                                   game_data.vessel_type, game_data.x,
+                                   game_data.y, game_data.z)
+
+
+@app.post("/shoot-at")
+async def shoot_at(game_data: ShootAtData) -> bool:
+    return game_service.shoot_at(game_data.game_id, game_data.shooter_name,
+                                 game_data.vessel_id, game_data.x,
+                                 game_data.y, game_data.z)
+
+
+@app.get("/game-status")
+async def get_game_status(game_id: int, player_name: str) -> str:
+    return game_service.get_game_status(game_id, player_name)
+
+
+@app.exception_handler(Exception)
+async def exception_handler(request: Request, exc: Exception):
+    return JSONResponse(status_code=500, content={"message": f"{exc}"})
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5000)
